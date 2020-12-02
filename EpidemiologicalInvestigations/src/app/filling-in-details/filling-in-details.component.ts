d437cf975a7fab81b6124bf63550cbf3b0015eaa
etishechter <0583229458E@GMAIL.COM>
2020-12-02 23:26:43 +0200

    תחילת הטופס

---
+++ EpidemiologicalInvestigations/src/app/filling-in-details/filling-in-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filling-in-details',
+  templateUrl: './filling-in-details.component.html',
+  styleUrls: ['./filling-in-details.component.css']
+})
+export class FillingInDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
